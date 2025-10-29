36be03cb9e2e1564b9f3de2bfa6b7aaa1efed2ff
Kingsley Ijomah <kingsley.ijomah@gmail.com>
2025-10-30 00:35:26 +0100

    fix issue with being able to type into txt fied

--- src/app/inbox/inbox.component.ts
+++ src/app/inbox/inbox.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inbox',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.scss']
 })
 export class InboxComponent {
   showList = false;
+  newItemText = '';
   
   inboxItems = [
     { id: 1, title: 'Review project proposal' },
