ad66026eee689557547a229730498c65ad187c69
Kingsley Ijomah <kingsley.ijomah@gmail.com>
2025-10-30 00:08:38 +0100

    Added inbox component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InboxComponent } from './inbox/inbox.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inbox',
+    component: InboxComponent
+  }
+];
